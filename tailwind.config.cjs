f858917a985fd2f41fd144cd17d8942a5cd1797b
Craig MacIntyre <craig.macintyre@thepetlabco.com>
2023-09-28 21:30:48 +0100

    added some colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				green: '#97C292',
+				yellow: '#FFDDAC',
+				pink: '#FFB8B7',
+				red: '#F4EEEE'
+			}
+		},
 	},
 	plugins: [],
 }
